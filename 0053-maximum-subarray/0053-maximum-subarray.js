ddb6b42e8a8ee8d17168049e14bc95e3998a5332
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-01-16 22:25:44 -0800

    Time: 81 ms (92.26%), Space: 49.6 MB (99.16%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let currentSum = nums[0];
-    let max = currentSum;
+    let maxSub = nums[0];
+    let currentSum = maxSub;
     
     for (let i = 1; i < nums.length; i++) {
         if (currentSum < 0) {
-            currentSum = 0;
+            currentSum = 0
         }
         
         currentSum += nums[i];
-        max = Math.max(currentSum, max)
+        maxSub = Math.max(currentSum, maxSub);
     }
     
-    return max;
+    return maxSub
 };
