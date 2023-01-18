803f97d72d811c7f3d35b27304ff1ace0c2a18f1
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-01-18 13:57:15 -0800

    Time: 65 ms (90.82%), Space: 42.4 MB (75.57%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -2,37 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var maxProduct = function(nums) {
-//     let currMin = nums[0];
-//     let currMax = nums[0];
-//     let max = nums[0];
-    
-//     for (let i = 1; i < nums.length; i++) {
-        
-//         let temp = currMax * nums[i]
-//         currMax = Math.max(currMax * nums[i], currMin * nums[i], nums[i]);
-//         currMin = Math.min(temp, currMin * nums[i], nums[i]);
-//         max = Math.max(max, currMax, currMin)
-//     }
-// };
-
 var maxProduct = function(nums) {
-    //initialize our variables with the first number in the array
     let max = nums[0];
-    let currMax = nums[0];
     let currMin = nums[0];
+    let currMax = nums[0];
     
-    //traverse through the nums array
     for (let i = 1; i < nums.length; i++) {
-        //make a temp variable that holds the currMax before we run the Math.min on currMin.
-        let temp = currMax * nums[i];
-        
-        currMax = Math.max(currMax * nums[i], nums[i], currMin * nums[i])
-        currMin = Math.min(temp, nums[i], currMin * nums[i])
-        
-        //find the max of max, currMax and currMin
-        max = Math.max(max, currMax, currMin)
-
+        let current = nums[i];
+        let temp = currMax * current;
+        currMax = Math.max(currMax * current, current, currMin * current)
+        currMin = Math.min(temp, current, currMin * current);
+        max = Math.max(max, currMax, currMin);
     }
     
     return max;
