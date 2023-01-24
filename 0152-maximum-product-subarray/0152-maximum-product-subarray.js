2d818167854ef319d9be561245c82f01ea8ec47d
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-01-24 10:44:35 -0800

    Time: 101 ms (30.26%), Space: 42.5 MB (67.57%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -4,15 +4,16 @@
  */
 var maxProduct = function(nums) {
     let max = nums[0];
-    let currMin = nums[0];
-    let currMax = nums[0];
+    let currentMin = nums[0];
+    let currentMax = nums[0];
     
     for (let i = 1; i < nums.length; i++) {
         let current = nums[i];
-        let temp = currMax * current;
-        currMax = Math.max(currMax * current, current, currMin * current)
-        currMin = Math.min(temp, current, currMin * current);
-        max = Math.max(max, currMax, currMin);
+        let temp = currentMax * current;
+        
+        currentMax = Math.max(currentMax * current, current, currentMin * current)
+        currentMin = Math.min(temp, current, currentMin * current)
+        max = Math.max(max, currentMax, currentMin)
     }
     
     return max;
