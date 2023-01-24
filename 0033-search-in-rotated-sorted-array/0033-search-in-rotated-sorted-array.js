1653468943c756445725ebee906d5bea20b43edc
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-01-24 13:49:23 -0800

    Time: 91 ms (28.77%), Space: 42.6 MB (13.42%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        
+        if (nums[middle] === target) {
+            return middle;
+        }
+        if (nums[left] <= nums[middle]) {
+            if (target > nums[middle] ||  target < nums[left]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        } else {
+            if (target < nums[middle] || target > nums[right]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+    }
+        
+
+    return -1;
+};
