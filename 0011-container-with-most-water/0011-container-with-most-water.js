c0588c300e27d614fea6db6400cd35281be1b940
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-02-03 22:35:14 -0800

    Time: 78 ms (81.51%), Space: 48.4 MB (99.62%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        const currentArea = Math.min(height[left], height[right]) * (right - left);
+        if (currentArea > maxArea) {
+            maxArea = currentArea
+        }
+        // maxArea = Math.max(maxArea, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+};
