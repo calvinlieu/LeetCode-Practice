58cbd825bf3d1d00b09ef72578dfc7cea9afce8c
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-02-06 22:27:41 -0800

    Time: 79 ms (77.50%), Space: 48.9 MB (88.27%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,26 +3,19 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    // two pointers + maxArea to keep track of max.
     let max = 0;
     let left = 0;
-    let right = height.length - 1; 
+    let right = height.length - 1;
     
-    
-    //basic two pointers while look
     while (left < right) {
+        let current = Math.min(height[left], height[right]) * (right - left);
         
-        // find the area by grabbing the minimum of both pointers times the width.
-        let area = Math.min(height[left], height[right]) * (right - left) 
-        
-        //if area is bigger then max, replace max with that area.
-        if (area > max) {
-            max = area;
+        if (current > max) {
+            max = current
         }
         
-        //move our pointers if one is smaller than the other.
         if (height[left] < height[right]) {
-            left++;
+            left++
         } else {
             right--;
         }
