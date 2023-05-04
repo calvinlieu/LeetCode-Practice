bdf086767e5adf50394836aa3e305aa1ee9a5ee5
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-05-04 16:23:52 -0700

    Time: 57 ms (86.70%), Space: 45.4 MB (29.30%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -1,25 +1,19 @@
+
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
 var search = function(nums, target) {
-    
-    //have 2 pointers at each end of the array;
     let left = 0;
     let right = nums.length - 1;
     
-    //normal binary search while loop;
     while (left <= right) {
-        
-        //grab our middle pointer;
         let middle = Math.floor((left + right) / 2);
         
-        //if the middle number is our target, return that index;
         if (nums[middle] === target) {
             return middle;
-            
-            //else if the middle number is less than the target, we want to search the right side.
         } else if (nums[middle] < target) {
             left = middle + 1;
         } else {
