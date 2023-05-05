2d08939ee231dd616c2a1de94a2be1ac06d726b6
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-05-04 22:22:18 -0700

    Time: 61 ms (45.76%), Space: 41.8 MB (92.25%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -11,23 +11,15 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    
-    //Depth First Search method
-    
-    
-    //base case
     if (root === null) return null;
     
-    //hold the left value at the current recursive call;
     let left = root.left;
-    //reassign both left and right nodes with each other;
     root.left = root.right;
     root.right = left;
     
-    //recursively call both the left and right subtrees;
+    
     invertTree(root.left);
     invertTree(root.right);
-    
-    //return the root at the end;
     return root;
 };
+
