ddccd81809da2f2291718641e8f443a5bd3440ab
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-05-18 22:50:51 -0700

    Time: 90 ms (56.97%), Space: 47.1 MB (62.58%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,22 +2,23 @@
  * @param {string} s
  * @return {number}
  */
+
 var lengthOfLongestSubstring = function(s) {
-    let set = new Set();
     let left = 0;
+    let set = new Set();
     let max = 0;
+    
     
     if (s.length === 0) return 0;
     if (s.length === 1) return 1;
     
     for (let i = 0; i < s.length; i++) {
-        while (set.has(s[i])) {
-            set.delete(s[left])
+        while(set.has(s[i])) {
+            set.delete(s[left]);
             left++;
         }
-        
-        set.add(s[i]);
-        max = Math.max(max, i - left + 1);
+        set.add(s[i])
+        max = Math.max (max, i - left + 1);
     }
     
     return max;
