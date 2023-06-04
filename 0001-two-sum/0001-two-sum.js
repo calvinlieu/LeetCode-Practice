3c407d548098bf24d22351ea3c16a058030dd187
Calvin Lieu <98150408+calvinlieu@users.noreply.github.com>
2023-06-03 23:50:49 -0700

    Time: 73 ms (62.12%), Space: 42.8 MB (40.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,17 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    //i have an input of arrays and an output of an array of two numbers that equal the target
-    // we would start off by creating a nest forloop so that we can go over each number and add them to see if they equal the target
-    
-    
-    for (i = 0; i < nums.length; i++) {
-        //we are using i + 1 to remove repetitive numbers
-        for (j = i + 1; j < nums.length; j++) {
-            //adding both i and j together and comparing it to the target. equality comparison.
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }   
+    let visited = {};
+    for (let i = 0; i < nums.length; i++) {
+        let current = nums[i];
+        let difference = target - current;
+        
+        if (difference in visited) {
+            return [visited[difference], i]
         }
+        
+        visited[current] = i
     }
 };
